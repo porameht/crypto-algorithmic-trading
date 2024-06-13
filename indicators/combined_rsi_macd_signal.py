5782f01e6f70968e7772ed1ad35bc3566fa404f8
fr4nk <porameht@outlook.com>
2024-06-13 19:39:28 +0700

    chore: Refactor combined_rsi_macd_signal function and adjust stop loss calculation

--- indicators/combined_rsi_macd_signal.py
+++ indicators/combined_rsi_macd_signal.py
@@ -24,17 +24,14 @@ def combined_rsi_macd_signal(session, symbol, timeframe):
     
     rsi = ta.momentum.RSIIndicator(kl.Close, window=20).rsi()
     macd = ta.trend.macd_diff(kl.Close)
-    atr = ta.volatility.AverageTrueRange(kl.High, kl.Low, kl.Close, window=14).average_true_range()
-    
-    atr_value = atr.iloc[-1]
     entry_price = kl.Close.iloc[-1]
     
     if rsi.iloc[-2] < 30 and rsi.iloc[-1] > 30 and macd.iloc[-1] > 0:
-        stop_loss = entry_price - atr_value  # Stop loss based on ATR
+        stop_loss = entry_price - 0.90  # Stop loss based on ATR
         take_profit, stop_loss = calculate_tp_sl(entry_price, stop_loss, risk_to_reward=1.5)
         return 'up', take_profit, stop_loss
     elif rsi.iloc[-2] > 70 and rsi.iloc[-1] < 70 and macd.iloc[-1] < 0:
-        stop_loss = entry_price + atr_value  # Stop loss based on ATR
+        stop_loss = entry_price + 0.90  # Stop loss based on ATR
         take_profit, stop_loss = calculate_tp_sl(entry_price, stop_loss, risk_to_reward=1.5)
         return 'down', take_profit, stop_loss
     else:
